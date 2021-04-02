1fe6aa751b63d16effed428e360668de1d0c50a1
karthik.selvaraj <karthik.selvaraj@avench.com>
2021-04-02 22:27:33 +0530

    added image and info text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import bgi from './assets/doorbg.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        BoilerPlate React Code for developing UI of the RESTROOM STATUS FINDER
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <img className="doorImage" src={bgi}/>
     </div>
   );
 }
